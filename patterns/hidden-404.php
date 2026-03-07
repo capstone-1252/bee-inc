117e857a5b65e85ac54d44c72dba465722a92111
rockin-potato-master <danielrist79@gmail.com>
2026-03-06 21:50:17 -0700

    removal of unneeded content #2
    hidden-404.php search bar not needed
    archive-product search tag for accessibility
    creation of page-contact
    page.html switch div to section

--- patterns/hidden-404.php
+++ patterns/hidden-404.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Title: 404
  * Slug: ellura-collections/404
@@ -12,7 +13,7 @@
 ?>
 <!-- wp:heading {"textAlign":"center","fontSize":"x-large"} -->
 <h2 class="wp-block-heading has-text-align-center has-x-large-font-size">
-	<?php esc_html_e( 'Page not found', 'ellura-collections' ); ?></h2>
+	<?php esc_html_e('Page not found', 'ellura-collections'); ?></h2>
 <!-- /wp:heading -->
-<!-- wp:paragraph --><p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try a search?', 'ellura-collections' ); ?></p><!-- /wp:paragraph -->
-<!-- wp:search {"label":"Search","showLabel":false,"buttonText":"Search"} /-->
+<!-- wp:paragraph -->
+<p><?php esc_html_e('It looks like nothing was found at this location. Maybe try a search?', 'ellura-collections'); ?></p><!-- /wp:paragraph -->
